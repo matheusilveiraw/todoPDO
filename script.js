3efb47876b4cd63f13fd72194437186bc360de26
Matheus <matheus_silveiraw@gmail.com>
2021-08-18 17:10:39 -0300

    cadastro funcionando

---
+++ script.js
@@ -0,0 +1,21 @@
+function chamarPg(url) { 
+
+    let ajax = new XMLHttpRequest();
+
+    ajax.open('GET', url)
+
+    ajax.onreadystatechange = () => { 
+        //console.log(ajax.readyState) 
+        if(ajax.readyState == 4 && ajax.status == 200) {
+            document.getElementById('conteudo').innerHTML = ajax.responseText
+        }
+
+        if(ajax.readyState == 4 && ajax.status == 404) {
+            //ERRO
+            document.getElementById('conteudo').innerHTML = 'tente novamente mais tarde'
+        }
+    }
+
+    ajax.send()
+
+}
